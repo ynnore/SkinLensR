81e8416de93d2e66621991f3c738fef3805818e0
ynnore <sooprar683@gmail.com>
2025-08-03 01:23:57 +0200

    fix: correct syntax error in user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,7 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)  # Ajouté
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String, nullable=False) renommé depuis hashed_password
+    hashed_password = Column(String, nullable=False)
     role = Column(String, default="user")
 
     legal_agreements = relationship("UserLegalAgreement", back_populates="user")
