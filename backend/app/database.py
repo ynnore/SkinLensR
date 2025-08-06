5cde272512293ec30141ccb617f7e389b1ee0d62
ynnore <sooprar683@gmail.com>
2025-08-06 10:57:32 +0200

    Corection: Suppression du secret via rewrite historique et ajout des env vars

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+# backend/app/database.py
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError('La variable d\'env DATABASE_URL n\'est pas définie.')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
